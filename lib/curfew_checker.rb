ab6435b5752e2d0586bdbe1e84f4939318f88ce7
meredithheller <meredithheller@outlook.com>
2018-08-07 19:22:15 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,9 +1,9 @@
-def simple_curfew_checker(time)
-  if time >= 11
-   "Past curfew"
-  else
-    "Not past curfew"
- end
-end
+# def simple_curfew_checker(time)
+#   if time >= 11
+#   "Past curfew"
+#   else
+#     "Not past curfew"
+# end
+# end
 
-puts simple_curfew_checker(10)
+# puts simple_curfew_checker(10)
